de93fa3d3e895c795f3622b5334c6ad7a8e442a5
Muhammad Iqbal Firdaus <iqbalfirdaus05@gmail.com>
2023-07-23 00:13:03 +0800

    create Comment models

---
+++ Midterm/models/comment.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  comment: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+commentSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
