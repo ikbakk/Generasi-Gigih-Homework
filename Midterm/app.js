06e75b95e97bf2c248616cc090feab305a23b947
Muhammad Iqbal Firdaus <iqbalfirdaus05@gmail.com>
2023-07-23 02:37:56 +0800

    change entry name

--- Midterm/app.js
+++ Midterm/app.js
@@ -1,33 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const { requestLogger, unknownEndpoint } = require('./utils/middleware');
 
-dotenv.config();
+const {
+  requestLogger,
+  unknownEndpoint,
+  errorLogger
+} = require('./utils/middleware');
 
-const databaseUrl = process.env.DB_URL;
+const commentRouter = require('./routes/comments');
 
 const app = express();
-
-mongoose
-  .connect(databaseUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
-mongoose.set('strictQuery', true);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(requestLogger);
+app.use(errorLogger);
+
+app.use('/api/comments', commentRouter);
 
 app.use(unknownEndpoint);
-
-module.exports = app;
