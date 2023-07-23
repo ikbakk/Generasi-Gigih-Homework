b62c00d1130abc5101560a97ddd9581f5390f32c
Muhammad Iqbal Firdaus <iqbalfirdaus05@gmail.com>
2023-07-23 16:14:46 +0800

    remove logger

--- Midterm/app.js
+++ Midterm/app.js
@@ -4,18 +4,13 @@ const bodyParser = require('body-parser');
 const connectToDB = require('./utils/database');
 const { commentsRouter, videosRouter, productsRouter } = require('./routes');
 
-const {
-  requestLogger,
-  unknownEndpoint,
-  errorHandler
-} = require('./utils/middleware');
+const { unknownEndpoint, errorHandler } = require('./utils/middleware');
 
 const app = express();
 const databaseUrl = process.env.DB_URL ?? '';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(requestLogger);
 
 app.use('/api/comments', commentsRouter);
 app.use('/api/videos', videosRouter);
