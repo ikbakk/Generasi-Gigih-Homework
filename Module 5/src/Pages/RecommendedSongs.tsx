55926533292f21c312ce639a8a3de23d91c59b6b
Muhammad Iqbal Firdaus <iqbalfirdaus05@gmail.com>
2023-08-04 20:14:32 +0800

    create RecommendedSongs page

---
+++ Module 5/src/Pages/RecommendedSongs.tsx
@@ -0,0 +1,17 @@
+import { Track } from "spotify-types";
+import Header from "../Components/Content/Header";
+import Songs from "../Components/Content/Songs";
+import useFetchRecommendedSongs from "../Hooks/useFetchRecommendedSongs";
+
+const RecommendedSongs = () => {
+  const { recommendedSongs } = useFetchRecommendedSongs();
+
+  return (
+    <div className="flex h-full w-full basis-full flex-col gap-4 justify-self-start overflow-hidden rounded-lg bg-primaryBlack p-4 md:basis-3/4">
+      <Header />
+      <Songs title="Search Results" tracks={recommendedSongs as Track[]} />
+    </div>
+  );
+};
+
+export default RecommendedSongs;
