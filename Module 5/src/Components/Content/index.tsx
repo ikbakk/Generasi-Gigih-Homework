a590c11826cddb6eabd32de27fcaccbec728b52d
Muhammad Iqbal Firdaus <iqbalfirdaus05@gmail.com>
2023-08-04 21:24:34 +0800

    add new children in route

--- Module 5/src/Components/Content/index.tsx
+++ Module 5/src/Components/Content/index.tsx
@@ -1,12 +1,11 @@
 import Categories from "./Categories";
 import Header from "./Header";
-import Songs from "./Songs";
 
 const Content = () => {
   return (
     <div className="flex h-full w-full basis-full flex-col gap-4 justify-self-start overflow-hidden rounded-lg bg-primaryBlack p-4 md:basis-3/4">
       <Header />
-      <Songs />
+      <Categories />
     </div>
   );
 };
--- Module 5/src/main.tsx
+++ Module 5/src/main.tsx
@@ -1,17 +1,22 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import MainLayout from "./Layouts/MainLayout.tsx";
 import SearchPage from "./Pages/Search.tsx";
 import RecommendedSongs from "./Pages/RecommendedSongs.tsx";
+import Content from "./Components/Content/index.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     children: [
+      {
+        path: "",
+        element: <Content />,
+      },
       {
         path: "/search",
         element: <SearchPage />,
