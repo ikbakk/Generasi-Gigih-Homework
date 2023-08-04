1d95d2375f42de79239102e83acd6694d7c140dc
Muhammad Iqbal Firdaus <iqbalfirdaus05@gmail.com>
2023-08-04 20:44:40 +0800

    reconfig main layout

--- Module 5/src/Layouts/MainLayout.tsx
+++ Module 5/src/Layouts/MainLayout.tsx
@@ -2,19 +2,16 @@ import { SongContextProvider } from "../Context/SongContext";
 import { LoginContextProvider } from "../Context/LoginContext";
 import Sidebar from "../Components/Sidebar";
 import MediaPlayer from "../Components/MediaPlayer";
+import { Outlet } from "react-router-dom";
 
-interface MainLayoutProps {
-  children: React.ReactNode;
-}
-
-const MainLayout = ({ children }: MainLayoutProps) => {
+const MainLayout = () => {
   return (
     <LoginContextProvider>
       <SongContextProvider>
         <div className="flex h-screen w-full flex-col overflow-hidden bg-black px-2 pt-2">
           <div className="flex h-[92%] w-full gap-2">
             <Sidebar />
-            {children}
+            <Outlet />
           </div>
           <MediaPlayer />
         </div>
--- Module 5/src/main.tsx
+++ Module 5/src/main.tsx
@@ -4,29 +4,28 @@ import App from "./App.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./Layouts/MainLayout.tsx";
-import Content from "./Components/Content/index.tsx";
 import SearchPage from "./Pages/Search.tsx";
 import RecommendedSongs from "./Pages/RecommendedSongs.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Content />,
-  },
-  {
-    path: "/search",
-    element: <SearchPage />,
-  },
-  {
-    path: "/recommended",
-    element: <RecommendedSongs />,
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/search",
+        element: <SearchPage />,
+      },
+      {
+        path: "/recommended",
+        element: <RecommendedSongs />,
+      },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MainLayout>
-      <RouterProvider router={router} />
-    </MainLayout>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
